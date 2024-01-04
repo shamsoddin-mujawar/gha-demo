a722e715ed0c9b8b0891f086629a4da9ca134a82
shamsoddin-mujawar <85243080+shamsoddin-mujawar@users.noreply.github.com>
2024-01-04 15:23:16 +0530

    Update demo.py

--- test_action/demo.py
+++ test_action/demo.py
@@ -1,1 +1,16 @@
+import os
+from common import common_methods
+
+
+def main():
+    input_a = os.getenv('A')
+    input_b = os.getenv('B')
+    add = common_methods.addition_of_two_variables(input_a, input_b)
+    print(f"addition of {input_a} and {input_b} is: {add} ")
+    with open(os.getenv("GITHUB_OUTPUT"), 'a') as fp:
+        fp.write("sum={}".format(add))
+
+
+if __name__ == "__main__":
+    main()
 
