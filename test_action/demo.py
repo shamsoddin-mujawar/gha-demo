ce0a809c767526ebec8c2d50efe32c486afa2bb3
shamsoddin-mujawar <85243080+shamsoddin-mujawar@users.noreply.github.com>
2024-01-04 15:23:54 +0530

    Update demo.py

--- test_action/demo.py
+++ test_action/demo.py
@@ -3,8 +3,8 @@ from common import common_methods
 
 
 def main():
-    input_a = os.getenv('A')
-    input_b = os.getenv('B')
+    input_a = int(os.getenv('A'))
+    input_b = int(os.getenv('B'))
     add = common_methods.addition_of_two_variables(input_a, input_b)
     print(f"addition of {input_a} and {input_b} is: {add} ")
     with open(os.getenv("GITHUB_OUTPUT"), 'a') as fp:
